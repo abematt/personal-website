7abf2c639ef9a4313e35b8765890a8c9ebfb843e
Abraham Mathew <abrahammathew.dev@gmail.com>
2025-06-19 18:38:53 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ['notion-client']
+    }
+  }
+  
+  module.exports = nextConfig

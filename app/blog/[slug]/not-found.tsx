c14a059adc4646287497641c56e251633c20569d
Abraham Mathew <abrahammathew.dev@gmail.com>
2025-06-22 23:51:18 +0200

    fix unescaped entities error

--- app/blog/[slug]/not-found.tsx
+++ app/blog/[slug]/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="prose prose-lg prose-invert mx-auto max-w-4xl text-center">
       <h1>Post Not Found</h1>
-      <p>The blog post you're looking for doesn't exist.</p>
+      <p>The blog post you are looking for does not exist.</p>
       <Link href="/blog" className="text-blue-400 hover:underline">
         ← Back to Blog
       </Link>
